4590dbe54512c29ec3cbba6b4cde7cfb45b4dc55
sakhdevel <sakhdevel@yandex.ru>
2022-09-05 19:43:46 +0300

    Добавлен сидер

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,10 +3,13 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Category;
+use App\Models\Item;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
+
     /**
      * Seed the application's database.
      *
@@ -14,11 +17,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $categoryPrinters = Category::factory()->create(["name" => "Принтеры"]);
+        $categoryOfficeAutomation = Category::factory()->create(["name" => "Оргтехника"]);
+        $categoryPens = Category::factory()->create(["name" => "Ручки"]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+
+        $itemPen = Item::factory()->create([
+            "name" => "Ручка",
+            "price" => 50,
+            "is_published" => false,
+            "is_deleted" => false,
+        ]);
+
+        $itemPrinter = Item::factory()->create([
+            "name" => "Принтер HP LaserJet 1300",
+            "price" => 5990,
+            "is_published" => true,
+            "is_deleted" => false,
+        ]);
+
+        $categoryPrinters->items()->attach($itemPrinter);
+        $categoryOfficeAutomation->items()->attach($itemPrinter);
+        $categoryPens->items()->attach($itemPen);
     }
 }
